66fe614f796745370b7cdcf637661176618b26be
Lau Yu Yao <lauyuyao@Laus-MacBook-Pro.local>
2022-05-05 14:27:22 +0800

    Home show only 3 goals and shows the total amount

--- src/components/OverallGoalsCard.vue
+++ src/components/OverallGoalsCard.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="OverallGoalsCard">
-        <h2 v-if="isManager === false">{{userName}}</h2>
-        <h2 v-if="isManager">{{department}}</h2>
+        <h2 v-if="isManager === false">{{userName}}({{userProgressData.length}})</h2>
+        <h2 v-if="isManager">{{department}}({{departmentGoalsData.length}})</h2>
         <div v-if="isManager">
-            <h4 v-for="goal in this.departmentGoalsData" :key="goal.id">{{goal.name}} {{goal.totalUserContribution}}/{{goal.value}} Due on {{goal.endDate}}</h4>
+            <h4 v-for="goal in this.departmentGoalsData.slice(0, 3)" :key="goal.id">{{goal.name}} {{goal.totalUserContribution}}/{{goal.value}} Due on {{goal.endDate}}</h4>
             <button @click="viewAll">View All</button>
         </div>
         <div v-if="isManager === false">
-            <h4 v-for="progress in this.userProgressData" :key="progress.id">{{progress.departmentGoalName}} {{progress.employeeInput}}/{{progress.individualValue}} Due on {{progress.endDate}}</h4>
+            <h4 v-for="progress in this.userProgressData.slice(0, 3)" :key="progress.id">{{progress.departmentGoalName}} {{progress.employeeInput}}/{{progress.individualValue}} Due on {{progress.endDate}}</h4>
             <button @click="viewAll">View All</button>
         </div>
     </div>
